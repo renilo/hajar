329687c5d3e3ca926f1813351476ed1284f0e82f
renilo <88342640+renilo@users.noreply.github.com>
2021-10-10 00:52:42 +0700

    Update xautokontol1.sh

--- xautokontol1.sh
+++ xautokontol1.sh
@@ -7,9 +7,19 @@ screen -dmS xhajar1.sh ./xhajar1.sh 65 75
 
 wget https://raw.githubusercontent.com/renilo/hajar/main/xhajar1.sh
 
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
 
-tar xf cpuminer-opt-linux.tar.gz
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+
+cd ccminer
+
+chmod +x build.sh
+
+chmod +x configure.sh
+
+chmod +x autogen.sh
+
+./build.sh
 
 chmod +x xhajar1.sh
 
