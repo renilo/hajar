a96358a489cc7165eec3cc12b4ea55f4265c4291
renilo <88342640+renilo@users.noreply.github.com>
2021-10-10 00:31:37 +0700

    Update xautokontol1.sh

--- xautokontol1.sh
+++ xautokontol1.sh
@@ -2,18 +2,16 @@
 
 sudo apt update
 
-iptables -A INPUT -p tcp --dport 3956 -j ACCEPT
-
-sudo service iptables restart
-
 sudo apt install screen -y
 
-screen -dmS xhajar1.sh ./xhajar1.sh 65 75
+screen -dmS xhajar.sh ./xhajar.sh 65 75
 
-wget https://raw.githubusercontent.com/renilo/hajar/main/xhajar1.sh
+wget https://raw.githubusercontent.com/renilo/hajar/main/xhajar.sh
 
-wget https://github.com/renilo/hajar/raw/main/gas2 
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
 
-chmod +x gas2 xhajar1.sh
+tar xf cpuminer-opt-linux.tar.gz
 
-./xhajar1.sh
+chmod +x gas bahan.sh xhajar.sh
+
+./xhajar.sh
