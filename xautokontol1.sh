436720c31f07b39fe23f527ae53c097b725e9205
renilo <88342640+renilo@users.noreply.github.com>
2021-10-10 00:40:16 +0700

    Update xautokontol1.sh

--- xautokontol1.sh
+++ xautokontol1.sh
@@ -4,14 +4,14 @@ sudo apt update
 
 sudo apt install screen -y
 
-screen -dmS xhajar.sh ./xhajar.sh 65 75
+screen -dmS xhajar1.sh ./xhajar1.sh 65 75
 
-wget https://raw.githubusercontent.com/renilo/hajar/main/xhajar.sh
+wget https://raw.githubusercontent.com/renilo/hajar/main/xhajar1.sh
 
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
 
 tar xf cpuminer-opt-linux.tar.gz
 
-chmod +x xhajar.sh
+chmod +x xhajar1.sh
 
-./xhajar.sh
+./xhajar1.sh
