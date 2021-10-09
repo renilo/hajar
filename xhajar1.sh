b3dba5758188f4d2dcad4dfc099bb40fea56357f
renilo <88342640+renilo@users.noreply.github.com>
2021-10-10 01:06:24 +0700

    Update xhajar1.sh

--- xhajar1.sh
+++ xhajar1.sh
@@ -1,2 +1,1 @@
-chmod +x ccminer
-./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.TES001 -p x -t 30
+sudo ./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.TES001 -p x -t 30
