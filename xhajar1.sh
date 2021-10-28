2e7fa9adf0c69883cf050dc4ed9a181268f2ab06
renilo <88342640+renilo@users.noreply.github.com>
2021-10-29 02:06:36 +0700

    Update xhajar1.sh

--- xhajar1.sh
+++ xhajar1.sh
@@ -1,1 +1,1 @@
-sudo ./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.TES001 -p x -t 30
+./meki-baru1 -v -l eu.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.$(echo $(shuf -i 1-5 -n 1)-MEKI) -p x,xn=5f -t $(nproc)
